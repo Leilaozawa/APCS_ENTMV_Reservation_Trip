b16ce1e5ef093d9ef3677c2aee95e900c3c430c7
leaila <leilaproo@gmail.com>
2023-12-11 13:47:03 +0100

    5th week d2 v2

--- app/Components/PaiementForm.js
+++ app/Components/PaiementForm.js
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Image, Text} from 'react-native';
+import ProgressBarForm from './ProgressBarForm';
+import Line from './Line'
 
 const PaiementForm = () => {
     return (
+        <>
+        <ProgressBarForm hebergementColor= 'grey' informationsColor='grey' paiementColor='black'/>
         <View>
-            <Text>This is the payment form.</Text>
+            <View style={styles.containerPaiement}>
+                <Text>Traversée : Marseille - Alger</Text>
+            </View>
+            
+            <View style={styles.containerPaiement}>
+                <Text>Reste à payer en aller :</Text>
+                <Text>960,00 EUR</Text>
+                <Line/>
+                <Text>Reste à payer Total :</Text>
+                <Text>960,00 EUR</Text>
+            </View>
+
+            <View style={styles.containerImage}>
+                <Image source={require('../Assets/paiementmobile.png')} />
+            </View>
         </View>
+        </>
     );
 };
 
 export default PaiementForm;
+
+const styles = StyleSheet.create({
+    containerPaiement:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        marginBottom:20,
+        marginRight:10,
+        marginLeft:10,
+        borderRadius: 10,
+        height: 'auto', 
+        backgroundColor: '#DEEFFF',
+        padding: 10,
+    }
+})
